33f9cfbbf2a809df3f4b03e1fa20f3e4a2ae2522
BryanRansil <bryan.ransil@gmail.com>
2023-04-21 15:48:38 -0700

    Make Coin Fountain on Tap
    Makes the adjustments necessary to see the coin fountain when we click on a scene. Notably I'm having to make the sizes incredibly small to just fit in my room, so should look into some general scale settings to map my game objects to real life space.

--- Assets/MainARSession.cs
+++ Assets/MainARSession.cs
@@ -1,11 +1,17 @@
 using Niantic.ARDK.AR;
 using Niantic.ARDK.AR.Configuration;
+using Niantic.ARDK.AR.HitTest;
+using Niantic.ARDK.Utilities;
+using Niantic.ARDK.Utilities.Input.Legacy;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainARSession : MonoBehaviour
 {
+    public Camera active_camera;
+    public GameObject spawn_prefab;
+
     IARSession _ar_session;
 
     // Start is called before the first frame update
@@ -26,6 +32,43 @@ public class MainARSession : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        // Check if the user's touched the screen
+        var touch = PlatformAgnosticInput.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            SpawnObject(touch);
+        }
+    }
+
+    void SpawnObject(Touch touch)
+    {
+        // If the ARSession isn't currently running, its CurrentFrame property will be null
+        var currentFrame = _ar_session.CurrentFrame;
+        if (currentFrame == null)
+            return;
+
+        // Hit test from the touch position
+        var results =
+            _ar_session.CurrentFrame.HitTest
+            (
+                active_camera.pixelWidth,
+                active_camera.pixelHeight,
+                touch.position,
+                ARHitTestResultType.All
+            );
+
+        if (results.Count == 0)
+            return;
+
+        var closestHit = results[0];
+        var position = closestHit.WorldTransform.ToPosition();
+
+        GameObject.Instantiate(spawn_prefab, position, Quaternion.identity);
     }
 }
