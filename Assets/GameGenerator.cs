3b90dd565ac6e1354df687247314939d0ffa7182
BryanRansil <bryan.ransil@gmail.com>
2023-06-23 13:14:51 -0700

    Create mountains with the terrain object
    An experiment in using terrain objects. Added a wooden frame (with an
    open source image) to make the edges not just end. Keeping on a separate
    branch because I'm not convinced this is the best approach for right
    now.

--- Assets/GameGenerator.cs
+++ Assets/GameGenerator.cs
@@ -4,6 +4,7 @@ using Niantic.ARDK.AR;
 using Niantic.ARDK.AR.HitTest;
 using Niantic.ARDK.Utilities;
 using System;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -13,20 +14,22 @@ using UnityEngine.UIElements;
 public class GameGenerator : MonoBehaviour
 {
     public GameObject boundary_prefab;
+    public List<GameObject> terrain_prefabs;
     public Camera game_camera;
 
     private IARFrame _current_frame;
     private Vector3 _furthest_boundary, _closest_boundary, _left_boundary, _right_boundary;
+    private GameObject _boundary;
     private GameObject _terrain;
     private Collider _terrain_collider;
     private float _terrain_level;
 
     // Generation Variables
     private float _height_tolerance = 10;
-    private bool _active;
+    private bool _on_cooldown;
     private float _increment_time;
     private const int _expansion_step = 40;
-    private readonly float game_growth_delta = 0.1f;
+    private readonly float pause_time = 3f;
 
     /**
      * From the screena and world positions, use the game_camera and current frame information to make a game
@@ -34,8 +37,10 @@ public class GameGenerator : MonoBehaviour
      */
     public void Populate(Vector3 worldPosition, IARFrame currentFrame)
     {
-        if (_active)
+        if (_on_cooldown)
             return;
+
+        _on_cooldown = true;
 
         _current_frame = currentFrame;
         bool expand_farther, expand_closer, expand_left, expand_right;
@@ -80,9 +85,9 @@ public class GameGenerator : MonoBehaviour
                                  _terrain_level,
                                  _left_boundary.z / 2 + _right_boundary.z / 2);
 
-        _terrain = GameObject.Instantiate(boundary_prefab, center, Quaternion.identity);
+        
+        _terrain = GameObject.Instantiate(terrain_prefabs[UnityEngine.Random.Range(0, 3)], center, Quaternion.identity);
         _terrain_collider = _terrain.GetComponent<Collider>();
-        _active = true;
     }
 
     private (Vector3, bool) ExpandGame(Vector3 boundary, Vector2 vector2)
@@ -117,20 +122,14 @@ public class GameGenerator : MonoBehaviour
 
     public void Update()
     {
-        if (!_active)
+        if (!_on_cooldown)
             return;
 
         _increment_time += Time.deltaTime;
 
-        if (_increment_time < game_growth_delta)
+        if (_increment_time < pause_time)
             return;
 
-        _increment_time = 0;
-        _terrain.transform.localScale += new Vector3(0.1f, 0.0f, 0.1f);
-        if (_terrain_collider.bounds.size.x >= Math.Abs(_furthest_boundary.x - _closest_boundary.x) ||
-            _terrain_collider.bounds.size.z >= Math.Abs(_left_boundary.z - _right_boundary.z))
-        {
-            _active = false;
-        }
+        _on_cooldown = false;
     }
 }
